312441f4f074a3c75bc4a8f71372e6f8ad5d4f32
Maco Filoteo <macofiloteo@gmail.com>
2020-08-10 05:16:32 -0700

    Fixed Static Files

--- library/urls.py
+++ library/urls.py
@@ -17,13 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
 from . import settings
+from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls'))
 ]
 
 if not settings.DEBUG:
-    urlpatterns += [
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': settings.STATIC_ROOT}),
-    ]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

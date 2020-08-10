b9dea62d0089db2dc20405a2fa8a2531edb6572c
Maco Filoteo <macofiloteo@gmail.com>
2020-08-10 04:36:51 -0700

    Final

--- api/books/serializers.py
+++ api/books/serializers.py
@@ -6,7 +6,7 @@ from .models import Book, Comment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = '__all__'
+        exclude = []
 
 class BookSerializer(serializers.ModelSerializer):
     book_comments = CommentSerializer(many=True, read_only=True)
